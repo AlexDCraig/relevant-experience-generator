6f4884d67158d386b938635e4b53ad55a6cdff48
AlexDHoffer <hoffera@oregonstate.edu>
2019-03-31 20:27:23 -0700

    Add template file and increase topics.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Parse LaTeX file, replace dummy values with correct ones: python parser.py
 
 Compile a LaTex file from .tex format, generate pdf: ./run_latex.ps1
 Assumes file is named 'main.tex'.
+
+'template.tex' is provided. This is a bare bones look at the illegal values of a resume. Illegal values here are simply values that are phony that we wish to replace with valid ones. In order to execute this suite properly, you need a file called 'proper_values.json' located within this same directory that maps the illegal values to the valid ones.
--- parser.py
+++ parser.py
@@ -3,6 +3,8 @@ import json
 import os
 
 full_path_to_file = None
+path_to_preserved_file = None
+label_titles = ['Heading', 'Education', 'Experience']
 
 def parse_cli_args():
     parser = argparse.ArgumentParser()
@@ -15,7 +17,6 @@ def parse_cli_args():
 def get_all_lines_from_file():
     all_lines = list()
     with open(full_path_to_file, 'r') as tex_file:
-        line_counter = 0
         for line in tex_file:
             all_lines.append(line)
     return all_lines
@@ -63,10 +64,10 @@ def log_back_proper_values(all_lines, label_lines):
         if label_lines.get(line_number):
             all_lines[line_number] = label_lines[line_number]
 
-def write_proper_values_to_file(corrected_lines):
-    with open(full_path_to_file, 'w') as tex_file:
-        for i in range(len(corrected_lines)):
-            line = corrected_lines[i]
+def write_values_to_file(lines, path):
+    with open(path, 'w') as tex_file:
+        for i in range(len(lines)):
+            line = lines[i]
             tex_file.write(line)
 
 if __name__ == "__main__":
@@ -75,23 +76,26 @@ if __name__ == "__main__":
     if file_name:
         dir_path = os.path.dirname(os.path.realpath(__file__))
         full_path_to_file = '{}\{}'.format(dir_path, file_name)
-
+        path_to_preserved_file = '{}\{}'.format(dir_path, file_name.replace('.', '-old.'))
         all_lines = get_all_lines_from_file()
+        preserved_lines = all_lines.copy()
 
-        # Get line number of header values.
-        label_title = 'Heading'
-        heading_label = '\label{{{}}}'.format(label_title)
-        heading_lines = get_label_lines(heading_label)
+        for label_title in label_titles:
+            # Get line number of values.
+            heading_label = '\label{{{}}}'.format(label_title)
+            heading_lines = get_label_lines(heading_label)
 
-        # Get a dictionary of 'improper values' mapped to their proper values, as defined by the user.
-        proper_values = get_proper_values(label_title)
+            # Get a dictionary of 'improper values' mapped to their proper values, as defined by the user.
+            proper_values = get_proper_values(label_title)
 
-        # We have a dictionary of lines mapping the label to values to be replaced. Complete the replacements.
-        search_and_replace_improper_values(heading_lines, proper_values)
+            if proper_values:
+                # We have a dictionary of lines mapping the label to values to be replaced. Complete the replacements.
+                search_and_replace_improper_values(heading_lines, proper_values)
 
-        # Log back the real values to the list representation of the .tex document.
-        log_back_proper_values(all_lines, heading_lines)
+                # Log back the real values to the list representation of the .tex document.
+                log_back_proper_values(all_lines, heading_lines)
 
-        # Write the proper values back to the tex file.
+        # Write the proper values back to the tex file and the old values to another tex file.
         if all_lines:
-            write_proper_values_to_file(all_lines)
+            write_values_to_file(all_lines, full_path_to_file)
+            write_values_to_file(preserved_lines, path_to_preserved_file)
